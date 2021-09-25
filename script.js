79ab1ce107e1a1dd061959e36eb786a8355b98f7
Gennadiy Bazarnov <bagbox@mail.ru>
2021-09-25 18:58:46 +0400

    task 2.2

--- script.js
+++ script.js
@@ -1,53 +1,73 @@
-let getBtn = document.querySelector('.cart-button');
-let getClass = document.querySelector('.goods-list');
-    getBtn.onclick = function() {
-    getClass.classList.toggle('display-none');
-};
+class Shawarma {
+    constructor(size, stuffing, topping) {
+      this.size = size;
+      this.stuffing = stuffing;
+      this.topping = topping;
+      this.checked = function (inputs, compliance) {   
+        let arr = [];     
+        inputs.forEach(el => {
+          if (el.checked) {
+            arr.push(compliance[el.value-1])} 
+        });         
+        return arr;              
+      };
+    }
+    // addTopping(topping) { 
+    // }   // Добавить добавку }
+    // removeTopping(topping) {
+    // } // Убрать добавку }
+    getSize() {   
+      let sizeCompliance = [{subPrice: 50, calorie: 20}, {subPrice: 100, calorie: 40}, {subPrice: 150, calorie: 60}];  
+      let shawarmaSizeInput = document.body.querySelectorAll("#shawarmaSize");     
+      this.size = this.checked(shawarmaSizeInput, sizeCompliance);    
+    };        // Узнать размер гамбургера }
+    
+    getStuffing() {  
+      let stuffCompliance = [{subPrice: 10, calorie: 20}, {subPrice: 20, calorie: 5}, {subPrice: 15, calorie: 10}]  
+      let stuffing = document.querySelectorAll("#stuffing");      
+      this.stuffing = this.checked(stuffing, stuffCompliance);
+    }        // Узнать начинку гамбургера }
 
-class GoodsItem {
-    constructor(title, price) {
-      this.title = title;
-      this.price = price;
-    }
-    render() {
-      return `<div class="goods-item"><h4>${this.title}</h4><p>$ ${this.price}</p></div>`;
-    }
-    TotalPrice(total) {
-        return `<div class="goods-item"><h3>Total Price :</h3><p>$ ${total}</p></div>`;
+    getToppings() {  
+      let toppingCompliance = [{subPrice: 0, calorie: 0}, {subPrice: 15, calorie: 5}, {subPrice: 0, calorie: 0}, {subPrice: 20, calorie: 5}, {subPrice: 40, calorie: 10}]  
+      let toppingSearch = document.querySelectorAll("#topping");      
+      this.topping = this.checked(toppingSearch, toppingCompliance);
+    } // Получить список добавок }     
+    
+    calculatePrice() { 
+      let totalPrice = calc(this.size) + calc(this.stuffing) + calc(this.topping); //  
+      function calc(params) {
+        if (!params.length) {
+          alert ('Эээ, брэт, начинка выбирай. Это обязательнэ!')
+        }
+         let summ = 0;
+        for (let i = 0; i < params.length; i++) {
+           summ += params[i].subPrice          
+        }   
+        return summ     
       }
+      return totalPrice;
+    }      // Узнать цену }
+    calculateCalories() {  
+      let totalCalories = calc(this.size) + calc(this.stuffing) + calc(this.topping); //  
+      function calc(params) {        
+        let summ = 0;
+        for (let i = 0; i < params.length; i++) {
+           summ += params[i].calorie          
+        }
+        return summ
+      }
+      return totalCalories
+    }  // Узнать калорийность }    
   }
+  const supaShawarma = new Shawarma ();
+  console.log ('Lets Go');
 
-class GoodsList {
-    constructor() {
-      this.goods = [];
-    }
-    fetchGoods() {
-      this.goods = [
-        { title: 'Shirt', price: 150 },
-        { title: 'Socks', price: 50 },
-        { title: 'Jacket', price: 350 },
-        { title: 'Shoes', price: 250 },
-      ];
-    }
-    render() {
-      let listHtml = '';
-      this.goods.forEach(good => {
-        const goodItem = new GoodsItem(good.title, good.price);
-        listHtml += goodItem.render();
-      });
-      document.querySelector('.goods-list').innerHTML = listHtml;
-    }
-    totalPrice() {
-        let totalPriceCount = 0;
-        this.goods.forEach(good => {
-            totalPriceCount += good.price          
-        });
-        const tp = new GoodsItem();        
-        document.querySelector('.goods-list').innerHTML += tp.TotalPrice(totalPriceCount);
-      }
-  };
-
-const list = new GoodsList();
-list.fetchGoods();
-list.render();
-list.totalPrice();
+  let getBtn = document.querySelector('.cart-button');
+    getBtn.onclick = function() {
+      supaShawarma.getSize();
+      supaShawarma.getStuffing();
+      supaShawarma.getToppings();
+      console.log (`Цена: ${supaShawarma.calculatePrice()} рублей. Калорийность: ${supaShawarma.calculateCalories()} кКал.`);  
+      ;
+    };
